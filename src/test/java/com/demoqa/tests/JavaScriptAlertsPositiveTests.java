0363adea8086d9389b4f990229d96524fa5b4f6b
IonCraciun19 <icraciu21@gmail.com>
2025-08-10 18:36:21 +0200

    Listener1

--- src/test/java/com/demoqa/tests/JavaScriptAlertsPositiveTests.java
+++ src/test/java/com/demoqa/tests/JavaScriptAlertsPositiveTests.java
@@ -5,6 +5,7 @@ import com.demoqa.pages.HomePage;
 import com.demoqa.pages.JavaScriptAlertsPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class JavaScriptAlertsPositiveTests extends TestBase {
@@ -21,17 +22,20 @@ public class JavaScriptAlertsPositiveTests extends TestBase {
 
     @Test
     @DisplayName("Click on -'JavaScriptAlerts'")
+    @Tag("smoky")
     public void clickOnJavaScriptAlertsPositiveTest(){
         jsAlertPage.checkH3JSText("JavaScript Alerts");
     }
 
     @Test
+    @Tag("smoky")
     public void iAmJSAlert(){
         jsAlertPage.verifyJSAlert();
     }
 
     @Test
     @DisplayName("Verify to 'OK' is displayed")
+    @Tag("smoky")
     public void alertAlertWithOKSelectResultTest(){
         jsAlertPage.selectResult("OK")
                 .verifyResult("Ok");
@@ -39,12 +43,14 @@ public class JavaScriptAlertsPositiveTests extends TestBase {
 
     @Test
     @DisplayName("Verify to 'Cancel' is displayed")
+    @Tag("smoky")
     public void alertAlertWithCancelSelectResultTest(){
         jsAlertPage.selectResult("Cancel")
                 .verifyResult("Cancel");
     }
 
     @Test
+    @Tag("smoky")
     public void sendMessageToAlertText(){
         jsAlertPage.sendMessageToAlert("Hallo Friends!!!")
                 .verifyMessage("Hallo Friends!!!");
